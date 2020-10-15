4874236f582723f0ef8d1d1164168da1460b7345
Dkadrie <dkadrie@uci.edu>
2020-10-15 01:04:21 -0700

    shell of db

--- DBaccess.cpp
+++ DBaccess.cpp
@@ -1,6 +1,24 @@
 #include <string> 
+#include <iostream>
+// TODO: create a student object with ID, Name, class list, units, several other things that I dont know what
 
-void insertStudent(int id, string name){
+//will eventually interface with a db, but now is just a placeholder.
+void insertStudent(int id, std::string name){
+    std::cout << name << std::endl;
+    std::cout << id << std::endl;
+}
 
+void removeStudent(int id){
+    std::cout << id << std::endl; 
+}
 
+//return student here when it is made
+void getStudent(int id){
+    std::cout << id << std::endl;
+}
+
+int main(){
+    std::string name = "Devin";
+    insertStudent(1234, name);
+    return 0;
 }
